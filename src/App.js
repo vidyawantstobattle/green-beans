118892c6161b7d3c4c04bb923a438b298a2edefa
Salsabeel Tantoush <salsabeelj66@gmail.com>
2024-06-12 01:46:10 +0300

    added dark mode

--- src/App.js
+++ src/App.js
@@ -1,35 +1,49 @@
 import { useState, useEffect } from "react";
 import Navigation from "./components/Navigation/Nav";
 import Home from "./pages/Home/home";
-
-
+import Contribute from "./pages/Contribute/contribute";
+import Community from "./pages/Community/community";
+import APIDocs from "./pages/APIDocs/apidocs";
+import Support from "./pages/Support/support";
+import Signup from "./pages/Signup/signup";
+import Login from "./pages/Login/login";
 import "./index.css";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
   const [currentPage, setCurrentPage] = useState(window.location.pathname);
 
-  useEffect(() => {
-    const handlePopState = () => {
-      setCurrentPage(window.location.pathname);
-    };
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
 
-    window.addEventListener('popstate', handlePopState);
-
-    return () => {
-      window.removeEventListener('popstate', handlePopState);
-    };
-  }, []);
 
   const renderPage = () => {
-    return <Home />;
-    // call your search page here 
+    switch (currentPage) {
+      case "/":
+        return <Home isDarkMode={isDarkMode} />;
+      case "/contribute":
+        return <Contribute />;
+      case "/community":
+        return <Community />;
+      case "/api-docs":
+        return <APIDocs />;
+      case "/support":
+        return <Support />;
+      case "/signup":
+        return <Signup />;
+      case "/login":
+        return <Login />;
+      default:
+        return <Home isDarkMode={isDarkMode} />;
+    }
   };
 
   return (
-    <>
-      <Navigation setCurrentPage={setCurrentPage} />
-      {renderPage()}
-    </>
+  <div className={isDarkMode ? 'app dark' : 'app'}>
+    <Navigation setCurrentPage={setCurrentPage} toggleDarkMode={toggleDarkMode} isDarkMode={isDarkMode} />
+    {renderPage()}
+  </div>
   );
 }
 
--- src/components/Navigation/Nav.js
+++ src/components/Navigation/Nav.js
@@ -1,16 +1,17 @@
 import "./Nav.css";
+import { useState } from "react";
 import { FiHeart } from "react-icons/fi";
 import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
-import logo from "../../assets/impactOS.jpeg";
+import logo from "../../assets/impactOS1.png";
 
-function Navigation({ setCurrentPage }) {
+function Navigation({ setCurrentPage, toggleDarkMode, isDarkMode }) {
   const navigate = (path) => {
     window.history.pushState(null, "", path);
     setCurrentPage(path);
   };
 
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${isDarkMode ? 'dark' : ''}`}>
       <div className="navbar-logo">
         <img src={logo} alt="askKauko" onClick={() => navigate("/")} />
       </div>
@@ -21,6 +22,9 @@ function Navigation({ setCurrentPage }) {
         <li><a href="#" onClick={() => navigate("/api-docs")}>API Docs</a></li>
         <li><a href="#" onClick={() => navigate("/support")}>Support</a></li>
       </ul>
+      <button className="dark-mode-toggle" onClick={toggleDarkMode}>
+        {isDarkMode ? "Light Mode" : "Dark Mode"}
+      </button>
     </nav>
   );
 }
