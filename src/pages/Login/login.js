2dd2d8f06c2d8982df3c0b93e86aa2523f95a76f
Salsabeel Tantoush <salsabeelj66@gmail.com>
2024-06-12 01:11:15 +0300

    added login and signup  pages

---
+++ src/pages/Login/login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <p>This is the Login page content.</p>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/pages/Signup/signup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Signup() {
+  return (
+    <div>
+      <h1>Sign Up Page</h1>
+      <p>This is the Sign Up page content.</p>
+    </div>
+  );
+}
+
+export default Signup;
