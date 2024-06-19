365b4a286d1ffd3ac3199c68ae6babe8d80c7ecc
Anusha A <anushaa@Anushas-MacBook-Air.local>
2024-06-19 13:54:57 +0300

    contribute form

--- src/pages/Contribute/contribute.js
+++ src/pages/Contribute/contribute.js
@@ -1,29 +1,61 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './contribute.css'; 
+import axios from 'axios';
 import Login from '../Auth/login';
 
 const Contribute = ({ darkMode, isLoggedIn, onLogin, setCurrentPage }) => {
   const [formData, setFormData] = useState({
-    emissionFactorValue: '',
-    emissionFactorUnit: '',
+    emission_quantity: '',
+    emission_quantity_units: '',
     pollutant: '',
-    dataSource: '',
-    geographicalScope: '',
-    emissionCategory: '',
-    co2OrCo2Equivalent: '',
-    technicalReference: '',
+    datasource: '',
+    geographicalscope: '',
+    emissioncategory: '',
+    co2_or_co2_equivalent: '',
+    technical_reference: '',
+    sector: '',
   });
+
+  const data = {
+    emissionCategories: [
+      { id: 1, name: 'Food and beverage' },
+      { id: 2, name: 'Fuel' },
+      { id: 72, name: 'Waste pellets' },
+      { id: 74, name: 'Municipal waste /mixed waste' }
+    ],
+    geographicalScopes: [
+      { id: 1, name: 'United States of America' },
+      { id: 2, name: 'Canada' },
+      { id: 10, name: 'Finland' }
+    ],
+    dataSources: [
+      { id: 1, name: 'IPCC' },
+      { id: 2, name: 'SYKE Finnish Environment Institute' },
+      { id: 3, name: 'Statistics Finland' }
+    ],
+    sector: [
+      { id: 1, name: 'Food and beverage' },
+      { id: 2, name: 'Energy' }
+    ]
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     console.log('Form submitted:', formData);
-    // required fields for the form to submit:
+    try {
+      const response = await axios.post('https://ec2-54-226-167-211.compute-1.amazonaws.com/api/emissionfactor/', formData); // Change the URL to your POST API endpoint
+      console.log('Form submitted:', response.data);
+      // Handle successful submission, e.g., show a success message or clear the form
+    } catch (error) {
+      console.error('Error submitting form:', error);
+      // Handle error state, e.g., show an error message
+    }
   };
 
   const currentPage=window.location.pathname;
@@ -46,23 +78,23 @@ const Contribute = ({ darkMode, isLoggedIn, onLogin, setCurrentPage }) => {
               <h2 className="form-title">Submit New Emission Factor</h2>
               <form onSubmit={handleSubmit}>
                 <div className="form-group">
-                  <label htmlFor="emissionFactorValue">Emission Factor Value:</label>
+                  <label htmlFor="emission_quantity">Emission Factor Value:</label>
                   <input
                     type="text"
-                    id="emissionFactorValue"
-                    name="emissionFactorValue"
-                    value={formData.emissionFactorValue}
+                    id="emission_quantity"
+                    name="emission_quantity"
+                    value={formData.emission_quantity}
                     onChange={handleChange}
                     required
                   />
                 </div>
                 <div className="form-group">
-                  <label htmlFor="emissionFactorUnit">Emission Factor Unit:</label>
+                  <label htmlFor="emission_quantity_units">Emission Factor Unit:</label>
                   <input
                     type="text"
-                    id="emissionFactorUnit"
-                    name="emissionFactorUnit"
-                    value={formData.emissionFactorUnit}
+                    id="emission_quantity_units"
+                    name="emission_quantity_units"
+                    value={formData.emission_quantity_units}
                     onChange={handleChange}
                     required
                   />
@@ -79,56 +111,83 @@ const Contribute = ({ darkMode, isLoggedIn, onLogin, setCurrentPage }) => {
                   />
                 </div>
                 <div className="form-group">
-                  <label htmlFor="dataSource">Data Source:</label>
+                  <label htmlFor="sector">Sector:</label>
+                  <select
+                  id="sector"
+                  name="sector"
+                  value={formData.sector}
+                  onChange={handleChange}
+                  required
+                >
+                  <option value="">Select Sector</option>
+                  {data.sector.map((source) => (
+                    <option key={source.id} value={source.id}>{source.name}</option>
+                  ))}
+                </select>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="datasource">Data Source:</label>
+                  <select
+                  id="datasource"
+                  name="datasource"
+                  value={formData.datasource}
+                  onChange={handleChange}
+                  required
+                >
+                  <option value="">Select Data Source</option>
+                  {data.dataSources.map((source) => (
+                    <option key={source.id} value={source.id}>{source.name}</option>
+                  ))}
+                </select>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="geographicalscope">Geographical Scope:</label>
+                  <select
+                  id="geographicalscope"
+                  name="geographicalscope"
+                  value={formData.geographicalscope}
+                  onChange={handleChange}
+                  required
+                >
+                  <option value="">Select Geographical Scope</option>
+                  {data.geographicalScopes.map((scope) => (
+                    <option key={scope.id} value={scope.id}>{scope.name}</option>
+                  ))}
+                </select>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="emissioncategory">Emission Category:</label>
+                  <select
+                  id="emissioncategory"
+                  name="emissioncategory"
+                  value={formData.emissioncategory}
+                  onChange={handleChange}
+                  required
+                >
+                  <option value="">Select Emission Category</option>
+                  {data.emissionCategories.map((category) => (
+                    <option key={category.id} value={category.id}>{category.name}</option>
+                  ))}
+                </select>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="co2_or_co2_equivalent">CO2 or CO2 Equivalent:</label>
                   <input
                     type="text"
-                    id="dataSource"
-                    name="dataSource"
-                    value={formData.dataSource}
+                    id="co2_or_co2_equivalent"
+                    name="co2_or_co2_equivalent"
+                    value={formData.co2_or_co2_equivalent}
                     onChange={handleChange}
                     required
                   />
                 </div>
                 <div className="form-group">
-                  <label htmlFor="geographicalScope">Geographical Scope:</label>
+                  <label htmlFor="technical_reference">Technical Reference:</label>
                   <input
                     type="text"
-                    id="geographicalScope"
-                    name="geographicalScope"
-                    value={formData.geographicalScope}
-                    onChange={handleChange}
-                    required
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="emissionCategory">Emission Category:</label>
-                  <input
-                    type="text"
-                    id="emissionCategory"
-                    name="emissionCategory"
-                    value={formData.emissionCategory}
-                    onChange={handleChange}
-                    required
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="co2OrCo2Equivalent">CO2 or CO2 Equivalent:</label>
-                  <input
-                    type="text"
-                    id="co2OrCo2Equivalent"
-                    name="co2OrCo2Equivalent"
-                    value={formData.co2OrCo2Equivalent}
-                    onChange={handleChange}
-                    required
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="technicalReference">Technical Reference:</label>
-                  <input
-                    type="text"
-                    id="technicalReference"
-                    name="technicalReference"
-                    value={formData.technicalReference}
+                    id="technical_reference"
+                    name="technical_reference"
+                    value={formData.technical_reference}
                     onChange={handleChange}
                     required
                   />
