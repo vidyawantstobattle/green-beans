f0308a35a92f21201effdefb083f335224d4a7ba
kiransaud <kiransaud49@gmail.com>
2024-06-20 17:33:01 +0300

    api docs added

--- src/pages/APIDocs/apidocs.js
+++ src/pages/APIDocs/apidocs.js
@@ -1,12 +1,47 @@
 import React from 'react';
+import { RedocStandalone } from 'redoc';
+// import SchemaTable from './SchemaTable';
 
-function APIDocs() {
+import './apidocs.css';
+
+const APIDocs = () => {
   return (
-    <div>
-      <h1>API Docs Page</h1>
-      <p>This is the API Docs page content.</p>
+    <div className="api-docs">
+      <header className="api-docs-header1">
+        <h1>Emission Factor API Documentation</h1>
+        {/* <p>Welcome to the Emission Factor API documentation. This page provides detailed information about the API endpoints, including descriptions and example requests.</p> */}
+      </header>
+
+      <section className="api-docs-intro">
+        <h2>About the API</h2>
+        <p>The Emission Factor API allows users to manage and retrieve emission factor data. It supports CRUD operations for contributors, data sources, geographical scopes, sectors, emission categories, and emission factors.</p>
+      </section>
+
+
+      <section className="api-docs-redoc">
+        <h2>API Explorer</h2>
+        <p>Use the interactive API explorer below to view the API endpoints. You can see detailed information and make real API requests.</p>
+        {/* <RedocStandalone
+          specUrl="/api-docs.json"
+          options={{
+            theme: { colors: { primary: { main: '#dd5522' } } },
+            hideDownloadButton: true,
+            expandResponses: "all"
+          }}
+        /> */}
+        <RedocStandalone specUrl="/api-docs.json" />
+
+      </section>
+            <section className="api-docs-database">
+              <h2>Database Models</h2>
+              <p>The following diagram illustrates the database models used in the Emission Factor API. Each table represents a different aspect of the data structure.</p>
+              <img src="/emissionfactor.png" alt="Database Models" className="api-docs-image"/>
+      
+            </section>
+      
     </div>
   );
-}
+};
 
 export default APIDocs;
+
